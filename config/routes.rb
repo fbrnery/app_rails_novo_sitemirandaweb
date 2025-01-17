5bb84cfc9150c0df0ddea16dbe7c0f86705ec52d
fabricio.miranda@apostou.com <fabricio.miranda@apostou.com>
2025-01-17 16:52:53 -0300

    Primeira parte do menu/header com direcionamento das urls internas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  get 'home/sobrenos'
+  get 'home/skills'
+  get 'home/portifolio_frontend'
+  get 'home/portifolio_ecommerces'
+  get 'home/sites_landingpages'
+  get 'home/ecommerces_lojas_virtuais'
+
+
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
